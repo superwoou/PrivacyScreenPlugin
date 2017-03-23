6659c849899d86f445dd22e793e4f588587d7a97
Yonghoon Lee <yhlee@team12.co.kr>
2017-03-23 17:03:54 +0900

    js update

--- www/PrivacyScreenPlugin.js
+++ www/PrivacyScreenPlugin.js
@@ -1,12 +1,14 @@
-var exec = require('cordova/exec');
+cordova.define("cordova/plugin/PrivacyScreenPlugin", function(require, exports, module) {
+    var exec = require('cordova/exec');
 
-var PrivacyScreenPlugin = {
-  activate : function(success, error) {
-    exec(success, error, "PrivacyScreenPlugin", "activate", []);
-  },
-  deactivate : function(success, error) {
-    exec(success, error, "PrivacyScreenPlugin", "deactivate", []);
-  }
-};
+    var PrivacyScreenPlugin = {
+      activate : function(success, error) {
+        exec(success, error, "PrivacyScreenPlugin", "activate", []);
+      },
+      deactivate : function(success, error) {
+        exec(success, error, "PrivacyScreenPlugin", "deactivate", []);
+      }
+    };
 
-module.exports = PrivacyScreenPlugin;
+    module.exports = PrivacyScreenPlugin;
+});
