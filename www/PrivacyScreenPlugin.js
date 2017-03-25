85179addf770ac40725b76a8a2c763b0de439ac8
Lee <superwoou@gmail.com>
2017-03-25 22:58:21 +0900

    js err

--- www/PrivacyScreenPlugin.js
+++ www/PrivacyScreenPlugin.js
@@ -1,14 +1,12 @@
-cordova.define("cordova/plugin/PrivacyScreenPlugin", function(require, exports, module) {
-    var exec = require('cordova/exec');
+var exec = require('cordova/exec');
 
-    var PrivacyScreenPlugin = {
-      activate : function(success, error) {
-        exec(success, error, "PrivacyScreenPlugin", "activate", []);
-      },
-      deactivate : function(success, error) {
-        exec(success, error, "PrivacyScreenPlugin", "deactivate", []);
-      }
-    };
+var PrivacyScreenPlugin = {
+  activate : function(success, error) {
+    exec(success, error, "PrivacyScreenPlugin", "activate", []);
+  },
+  deactivate : function(success, error) {
+    exec(success, error, "PrivacyScreenPlugin", "deactivate", []);
+  }
+};
 
-    module.exports = PrivacyScreenPlugin;
-});
+module.exports = PrivacyScreenPlugin;
